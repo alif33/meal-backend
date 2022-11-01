9b7b87af0eb2665cb7df835a97bea671f101fc53
alif33 <alifhasan332@gmail.com>
2022-11-01 13:34:17 +0600

    ...

--- routes/resturantRoutes.js
+++ routes/resturantRoutes.js
@@ -10,6 +10,13 @@ const {
    addDeliveryZone,
    updateDeliveryZone
  } = require("../controller/deliveryZoneController");
+ 
+const { 
+   getPhotos,
+   addPhoto,
+   dropPhoto
+  //  updateDeliveryZone
+ } = require("../controller/photoController");
 
 
 router.get('/resturant/coupons', getCoupons);
@@ -19,5 +26,10 @@ router.put('/resturant/coupon', updateCoupon);
 router.get('/resturant/delivery-zones', getDeliveryZones);
 router.post('/resturant/delivery-zone', addDeliveryZone);
 router.put('/resturant/delivery-zone', updateDeliveryZone);
+
+router.get('/resturant/photos', getPhotos);
+router.post('/resturant/photo', addPhoto);
+router.delete('/resturant/photo', dropPhoto);
+// router.put('/resturant/delivery-zone', updateDeliveryZone);
 
 module.exports = router;
